7dd43b326c90526b35aefc04267104801f024ae1
OPBS Liyana <liyana.perso@gmail.com>
2026-06-09 09:33:09 -0400

    CALC HEIGH 100 - HEADER

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-DINnssLg.css"], false, './mount');
-      return __federation_import('/appmenu-dist/assets/__federation_expose_Mount-wucRn8Dr.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-CghojNnB.css"], false, './mount');
+      return __federation_import('/appmenu-dist/assets/__federation_expose_Mount-B8WFyYEb.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
