b21bb47d08f4eef4575dc01781d566c1545137e6
OPBS Liyana <liyana.perso@gmail.com>
2026-06-09 15:26:37 -0400

    visual test for menu

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-CghojNnB.css"], false, './mount');
-      return __federation_import('/appmenu-dist/assets/__federation_expose_Mount-B8WFyYEb.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-DZJVsRAo.css"], false, './mount');
+      return __federation_import('/appmenu-dist/assets/__federation_expose_Mount-m-8wfifP.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
