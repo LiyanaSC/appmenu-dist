d4fdb986072c20b8cdefcb6e2addd8f860c135ae
OPBS Liyana <liyana.perso@gmail.com>
2026-06-18 14:37:05 -0400

    bdd connexion

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-BYOWbeCx.css"], false, './mount');
-      return __federation_import('/appmenu-dist/assets/__federation_expose_Mount-D-505NFe.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-OFHZeXPX.css"], false, './mount');
+      return __federation_import('/appmenu-dist/assets/__federation_expose_Mount-Bnka_vX0.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
